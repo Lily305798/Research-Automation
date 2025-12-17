53a62aac849de57ba1e25695daf03ff39ca5712a
L3l7 <prettya997@gmail.com>
2025-12-17 11:38:43 -0500

    [UPDATE] : Setting up openvas, second entrypoint.sh and wip shared volume

---
+++ 2025_05_openvas_report/gvmd_build/entrypoint.openvas-scanner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "[+] Preparing runtime directories"
+mkdir -p /var/run/openvas
+mkdir -p /var/lib/openvas/plugins
+
+echo "[+] Starting Redis"
+redis-server --daemonize yes
+
+redis-cli ping
+echo "[+] Redis is ready"
+
+echo "[+] Starting openvas-scanner"
+
+exec openvas \
+  --foreground \
+  --unix-socket=/var/run/openvas/openvas.sock
